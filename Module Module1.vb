26220fe9277c768d7d28cc64ed5bd2196ff98e64
Noah22550 <noahdepagne@gmail.com>
2025-09-08 13:38:12 +0200

    Add files via upload

---
+++ Module Module1.vb
@@ -0,0 +1,16 @@
+Module Module1
+
+    Sub Main()
+        Dim note As Integer
+        Dim moyenne As Double
+        Dim pourcentage As Double
+        While note <> -1
+            Console.WriteLine("Entrez une note (-1) pour fin ")
+            note = Console.ReadLine()
+        End While
+        pourcentage =
+        Console.WriteLine("Vous avez") : pourcentage.ToString("de notes > à 10 ")
+        pourcentage = Console.ReadLine()
+        moyenne = 
+    End Sub
+End Module
